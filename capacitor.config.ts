6760d94a7ae35c86f6e3360fffb19f6ddc161d6d
Justin Hallier <justin.hallier+git@gmail.com>
2025-07-20 21:51:38 +1200

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'xyz.jallier.readecksharer',
+  appName: 'readeck-sharer',
+  webDir: 'build',
+};
+
+export default config;
